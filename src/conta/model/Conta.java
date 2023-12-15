98786dd29ca051530f7f57c8f16ef26ae47ef84c
igor <igor_bolsistasoftex@procenge.com.br>
2023-12-15 16:32:06 -0300

    Criação ContaPoupanca e ContaCorrente

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -54,4 +54,45 @@ public class Conta {
 	public void setSaldo(float saldo) {
 		this.saldo = saldo;
 	}
+	
+	public boolean sacar(float valor) {
+		if(this.getSaldo()<valor) {
+		System.out.println("\n Saldo Insuficiente!");
+		return false;
+		}
+		
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+	}
+	
+	public void depositar(float valor) {
+		this.setSaldo(this.getSaldo() + valor);
+	}
+	
+	public void visualizar() {
+		
+		String tipo = "";
+		
+		switch(this.tipo) {
+		case 1:
+			tipo = "Conta Corrente";
+			break;
+		case 2:
+			tipo = "Conta Poupança";
+			break;
+		}
+		
+		System.out.println("\n\n**************************************************************************");
+		System.out.println("Dados da Conta");
+		System.out.println("\n\n**************************************************************************");
+		System.out.println("Número da Conta: " + this.numero);
+		System.out.println("Agência: "+ this.agencia);
+		System.out.println("Tipo da conta: " + tipo);
+		System.out.println("Titular: " + this.titular);
+		System.out.println("Saldo: " + this.saldo);
+		
+		
+	}
+	
+	
 }
